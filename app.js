66fee65be7d69166f87bd45056af4f442fc58a7c
mhdaloux2 <mhdaloux2@gmail.com>
2019-11-06 12:50:41 +0100

    [Express] Authentication | User creation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('./db/db');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
